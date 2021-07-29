a4ce696015a91d4966065d4b1c41f3f147bbf3d6
dprats <diego.prats@gmail.com>
2021-07-29 12:28:41 -0700

    make the index.js file react

--- src/icp_ecosystem_assets/src/index.js
+++ src/icp_ecosystem_assets/src/index.js
@@ -1,9 +1,40 @@
+import * as React from "react";
+import { render } from "react-dom";
 import { icp_ecosystem } from "../../declarations/icp_ecosystem";
 
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with icp_ecosystem actor, calling the greet method
-  const greeting = await icp_ecosystem.greet(name);
+const MyHello = () => {
+  const [name, setName] = React.useState('');
+  const [message, setMessage] = React.useState('');
 
-  document.getElementById("greeting").innerText = greeting;
-});
+  async function doGreet() {
+    const greeting = await custom_greeting.greet(name);
+    setMessage(greeting);
+  }
+
+  return (
+    <div style={{ "fontSize": "30px" }}>
+      <div style={{ "backgroundColor": "yellow" }}>
+        <p>Greetings, from DFINITY!</p>
+        <p>
+          {" "}
+          Type your message in the Name input field, then click{" "}
+          <b> Get Greeting</b> to display the result.
+        </p>
+      </div>
+      <div style={{ margin: "30px" }}>
+        <input
+          id="name"
+          value={name}
+          onChange={(ev) => setName(ev.target.value)}
+        ></input>
+        <button onClick={doGreet}>Get Greeting!</button>
+      </div>
+      <div>
+        Greeting is: "
+        <span style={{ color: "blue" }}>{message}</span>"
+      </div>
+    </div>
+  );
+};
+
+render(<MyHello />, document.getElementById("app"));
